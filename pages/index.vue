4a7445846f8ab76be27ae178552f14b6dbd112ac
jide_omotola <jide@reach.africa>
2021-06-10 12:45:34 +0100

    added aboutcafeone & take a tour sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
+    <aboutCafeone />
+    <takeTour />
     <cafeLocations />
   </div>
 </template>
 <script>
-import cafeLocations from "@/components/Homepage/cafeLocations.vue"
+  import aboutCafeone from "@/components/Homepage/aboutCafe.vue";
+  import takeTour from "@/components/Homepage/takeTour.vue";
+  import cafeLocations from "@/components/Homepage/cafeLocations.vue";
   export default {
     components: {
+      aboutCafeone,
+      takeTour,
       cafeLocations
     }
   }
