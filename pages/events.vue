b738adcfadc8194bcab0ee232fb96c468ba3e9cf
jide_omotola <jide@reach.africa>
2021-06-14 07:18:37 +0100

    added pastevnts components

---
+++ pages/events.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <pastEvents />
+  </div>
+</template>
+<script>
+  import pastEvents from "@/components/Events/pastEvents.vue";
+  export default {
+    components: {
+      pastEvents,
+    }
+  }
+
+</script>
+<style scoped>
+
+</style>
