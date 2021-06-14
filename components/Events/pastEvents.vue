7d1544d5a3a0a18a7f7e3a702d4781aff5224ead
jide_omotola <jide@reach.africa>
2021-06-14 08:08:56 +0100

    added past events section for events page

--- components/Events/pastEvents.vue
+++ components/Events/pastEvents.vue
@@ -2,31 +2,166 @@
   <div class="upcomingevents">
     <div class="container upcomingevents-body col justify-content-between">
       <div>
-        <span class="events">UPCOMING EVENTS &nbsp;<img src="@/assets/img/events-marker.svg" alt="event marker"></span>
-        <h2>Café One Talk And<br /> Events</h2>
+        <span class="events">PAST EVENTS &nbsp;<img src="@/assets/img/events-marker.svg" alt="event marker"></span>
+        <h2>Some Text About Events That<br /> Have Happened Comes Here</h2>
       </div>
-      <div class="tables text-start">
-        <slider />
+      <div class="tables text-start pt-5">
+        <div class="container col pb-5 position-relative pr-0 pl-0">
+          <div class="carousel position-relative" data-ride="carousel">
+            <!-- The slideshow -->
+            <div class="carousel-inner">
+              <div class="carousel-item carousel-item-next carousel-item-left">
+                <div class="d-flex justify-content-between">
+                  <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column pl-2 pr-2">
+                    <a href="#" class="introduce">
+                      <div class="flex-column-1 ">
+                        <img src="@/assets/img/event-1.jpeg" alt="img-1">
+                      </div>
+                      <div class="flex-column-2-red">
+
+                      </div>
+                    </a>
+                  </div>
+                  <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column pl-2 pr-2">
+                    <a href="#" class="introduce">
+                      <div class="flex-column-1 ">
+                        <img src="@/assets/img/event-2.jpeg" alt="img-2">
+                      </div>
+                      <div class="flex-column-2-grey">
+
+                      </div>
+                    </a>
+                  </div>
+                  <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column pl-2 pr-2">
+                    <a href="#" class="introduce">
+                      <div class="flex-column-1 ">
+                        <img src="@/assets/img/event-3.jpeg" alt="img-3">
+                      </div>
+                      <div class="flex-column-2-red">
+
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-inner pt-5">
+              <div class="carousel-item carousel-item-next carousel-item-left">
+                <div class="d-flex justify-content-between">
+                  <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column pl-2 pr-2">
+                    <a href="#" class="introduce">
+                      <div class="flex-column-1 ">
+                        <img src="@/assets/img/event-1.jpeg" alt="img-1">
+                      </div>
+                      <div class="flex-column-2-red">
+
+                      </div>
+                    </a>
+                  </div>
+                  <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column pl-2 pr-2">
+                    <a href="#" class="introduce">
+                      <div class="flex-column-1 ">
+                        <img src="@/assets/img/event-2.jpeg" alt="img-2">
+                      </div>
+                      <div class="flex-column-2-grey">
+
+                      </div>
+                    </a>
+                  </div>
+                  <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column pl-2 pr-2">
+                    <a href="#" class="introduce">
+                      <div class="flex-column-1 ">
+                        <img src="@/assets/img/event-3.jpeg" alt="img-3">
+                      </div>
+                      <div class="flex-column-2-red">
+
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-  import slider from "@/components/slider.vue";
   export default {
-    name: "upcomingEvents",
+    name: "pastEvents",
     data() {
       return {}
     },
-    components: {
-      slider
-    }
+
   };
 
 </script>
 <style lang="scss" scoped>
   @import "@/scss/abstracts/_variables";
   @import "@/scss/abstracts/_mixins";
+
+  .introduce {
+    height: 22.5rem;
+    background-color: white;
+    box-shadow: 0 3px 10px #5969f642;
+    color: black;
+  }
+
+  .introduce:hover {
+    box-shadow: 0 3px 15px #5969f666;
+    color: black;
+    text-decoration: none;
+  }
+
+  .flex-column-1 {
+    height: 18rem;
+    width: 100%;
+  }
+
+  .flex-column-2-red {
+    height: 4.5rem;
+    width: 100%;
+    align-items: center;
+    background-color: #BC2934;
+  }
+
+  .flex-column-2-grey {
+    height: 4.5rem;
+    width: 100%;
+    align-items: center;
+    background-color: #474E59;
+  }
+
+  .pic {
+    height: 70px;
+    width: 70px;
+    margin-left: 20px;
+    border-radius: 50%;
+    transition: all .2s ease-in-out;
+  }
+
+  .introduce:hover .pic {
+    transform: scale(1.1);
+  }
+
+  .carousel-indicators .active {
+    background-color: #BC2934;
+    ;
+  }
+
+  .carousel-indicators li {
+    background-color: #c8e5ff;
+  }
+
+  .carousel-indicators {
+    bottom: -50px;
+  }
+
+  .flex-column-1 img {
+    width: 100%;
+    height: 18rem;
+  }
 
   .upcomingevents-text {
     font-size: 20px;
