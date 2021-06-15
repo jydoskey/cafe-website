1b1d5b541b0d8edf833c63bc70fbdebcc8278bb1
jide_omotola <jide@reach.africa>
2021-06-15 05:30:42 +0100

    updated font-family

--- components/CoWorking/gallery.vue
+++ components/CoWorking/gallery.vue
@@ -151,6 +151,7 @@
     letter-spacing: normal;
     color: #474E59;
     text-align: start;
+    font-family: Lora;
   }
 
   .tables {
@@ -209,6 +210,7 @@
     color: #222831;
     margin: 0;
     text-transform: uppercase;
+    font-family: Lato;
   }
 
   .introduce {
