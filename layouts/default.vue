a231fb45fa6dc5157343e83faf19d393c2604b58
Tomiwa Ajayi <ayooluwatomiwaajayi@gmail.com>
2021-06-16 04:54:42 +0100

    add navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,79 +1,79 @@
 <template>
   <div>
+    <navBar />
     <Nuxt />
     <app-footer />
   </div>
 </template>
 
 <script>
-  import appFooter from "@/layouts/appFooter.vue"
-  export default {
-    components: {
-      appFooter
-    }
-  }
-
+import appFooter from "@/layouts/appFooter.vue";
+import navBar from "@/layouts/navBar.vue";
+export default {
+  components: {
+    appFooter,
+    navBar,
+  },
+};
 </script>
 
 <style lang="scss">
-  @import "@/scss/vendor/themed-bootstrap.scss";
-  @import "@/scss/main.scss";
+@import "@/scss/vendor/themed-bootstrap.scss";
+@import "@/scss/main.scss";
 
+body {
+  overflow-x: hidden !important;
+}
 
-  body {
-    overflow-x: hidden !important;
-  }
+.app-body {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  min-height: 100vh;
+  margin: 0 auto;
+}
 
-  .app-body {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    min-height: 100vh;
-    margin: 0 auto;
-  }
+.app-main {
+  flex: 1 0 auto;
+}
 
-  .app-main {
-    flex: 1 0 auto;
-  }
+.btn-lilac {
+  background-color: #c82b81;
+  color: #fff;
+  width: 242px;
+  height: 71px;
+  padding: 14px 46px 23px;
+}
 
-  .btn-lilac {
-    background-color: #c82b81;
-    color: #fff;
-    width: 242px;
-    height: 71px;
-    padding: 14px 46px 23px;
-  }
+.btn {
+  border-radius: 10px;
+}
 
-  .btn {
-    border-radius: 10px;
-  }
+.btn-green {
+  background-color: #29b36a;
+  color: #fff;
+}
 
-  .btn-green {
-    background-color: #29b36a;
-    color: #fff;
-  }
+.btn-red {
+  background-color: #bc2934;
+  color: #fff;
+}
 
-  .btn-red {
-    background-color: #BC2934;
-    color: #fff;
-  }
+.btn-orange {
+  background: #ff565c;
+  color: #fff;
+}
 
-  .btn-orange {
-    background: #ff565c;
-    color: #fff;
-  }
+.no-gap {
+  padding-right: 0;
+  padding-left: 0;
+}
 
-  .no-gap {
-    padding-right: 0;
-    padding-left: 0;
-  }
+.top-page {
+  margin-top: 78px;
 
-  .top-page {
+  @include breakpoint-min(md) {
     margin-top: 78px;
-
-    @include breakpoint-min(md) {
-      margin-top: 78px;
-    }
   }
-
+}
 </style>
---
+++ layouts/navBar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="navbar my-0" :class="{ change_color: scrollPosition > 80 }">
+    <div class="container">
+      <div class="logo">
+        <NuxtLink to="/">
+          <img src="../assets/cafe-one-logo.svg" alt="" />
+        </NuxtLink>
+      </div>
+      <div class="links">
+        <div class="about mx-2">
+          <NuxtLink to="/about" class="nav-link d-inline-block py-2 px-2"
+            >About</NuxtLink
+          >
+        </div>
+        <div class="co-working mx-2">
+          <NuxtLink to="/co-working" class="nav-link d-inline-block py-2 px-2"
+            >Co-Working</NuxtLink
+          >
+        </div>
+        <div class="events mx-2">
+          <NuxtLink to="/events" class="nav-link d-inline-block py-2 px-2"
+            >Events</NuxtLink
+          >
+        </div>
+        <div class="community mx-2">
+          <NuxtLink to="/community" class="nav-link d-inline-block py-2 px-2"
+            >Community</NuxtLink
+          >
+        </div>
+        <div class="cafe mx-2">
+          <NuxtLink to="/cafe" class="nav-link d-inline-block py-2 px-2"
+            >Cafe</NuxtLink
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    scrollPosition: 0,
+  }),
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  /* background: #000; */
+  position: sticky;
+  top: 0;
+  z-index: 50000000;
+
+  &.change_color {
+    background: #000;
+    /* z-index: 100;
+	box-shadow: 0px -3px 12px 8px rgba(0, 0, 0, 0.13);
+	box-shadow: 0px 5px 5px 1px rgba(0, 0, 0, 0.2); */
+  }
+
+  .links {
+    display: flex;
+
+    a {
+      color: #fff;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        text-decoration: none;
+        border-bottom: 3px solid #bc2934;
+        transition: 1s ease all;
+      }
+    }
+  }
+
+  .nav-link.nuxt-link-exact-active {
+    border-bottom: 3px solid #bc2934;
+  }
+}
+</style>
