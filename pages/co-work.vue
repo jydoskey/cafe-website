da61fa601e605bdb853a8b7cb035784cc5071732
jide_omotola <jide@reach.africa>
2021-06-17 01:13:50 +0100

    added our space section to co-working page

--- pages/co-work.vue
+++ pages/co-work.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <ourSpace />
     <subscription />
     <bookSpace />
     <gallery />
   </div>
 </template>
 <script>
+  import ourSpace from "@/components/CoWorking/ourSpace.vue";
   import subscription from '@/components/CoWorking/subscription.vue';
   import bookSpace from "@/components/CoWorking/bookSpace.vue";
   import gallery from "@/components/CoWorking/gallery.vue";
   export default {
     components: {
+      ourSpace,
       subscription,
       bookSpace,
       gallery
