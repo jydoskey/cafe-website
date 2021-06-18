1c63feaaaa5ee604a2a05565fac9f60f86fe3f00
jide_omotola <jide@reach.africa>
2021-06-18 14:51:09 +0100

    created co-working space hero section

--- components/CoWorking/heroSection.vue
+++ components/CoWorking/heroSection.vue
@@ -1,11 +1,17 @@
 <template>
   <section class="hero-section">
-    <div class="container">
+    <div class="container-fluid">
       <div class="row">
         <div class="col-lg-5">
-          <h1 class="hero-section__header">Interested In <span class="text-red">Working</span> From A Creative
+          <h1 class="hero-section__header">Interested In <span class="text-red">Working</span><br /> From A
+            Creative<br />
             Environment?</h1>
-          <p class="text-steel hero-section__description">Our Co-working space is for you</p>
+          <p class="hero-section__description">Our Co-working space is for you</p>
+          <div class="text-center pt-2">
+            <button type="submit" id="submit" name="submit" class="col-8 btn btn-lg btn-block btn-red  mr-auto ml-auto">
+              BOOK A SPACE
+            </button>
+          </div>
         </div>
         <div class="col-lg-7">
           <div class="hero-image">
@@ -31,6 +37,17 @@
   .hero-section {
     position: relative;
     padding-top: 9rem;
+    padding-bottom: 4rem;
+
+    @include breakpoint-min(lg) {
+      padding-bottom: 7rem;
+    }
+  }
+
+  .btn-red {
+    border-radius: 0% !important;
+    font-size: 16px;
+    height: 3.5rem;
   }
 
   .hero-image {
@@ -38,32 +55,55 @@
     height: 20rem;
     background-position: inherit;
     background-repeat: no-repeat;
-    background-size: contain;
+    background-size: cover;
 
     @include breakpoint-min(lg) {
       top: 3rem;
       position: relative;
       width: 100%;
-      height: 25rem;
+      max-width: 720px;
+      height: 26rem;
+    }
+  }
+
+  .col-lg-7 {
+    @include breakpoint-min (lg) {
+      padding: 0;
     }
   }
 
   .hero-section__header {
     margin-bottom: 2.5rem;
-    text-align: center;
+    font-family: Lora;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 3.25rem;
+    line-height: 67px;
+    color: #474E59;
 
     @include breakpoint-min(lg) {
       text-align: start;
+      left: 4rem;
+      position: relative;
       margin-bottom: 3rem;
+      width: 40rem;
+    }
+
+    @include breakpoint-max(lg) {
+      text-align: center;
     }
   }
 
   .hero-section__description {
-    margin-bottom: 4rem;
     text-align: center;
+    font-family: Lato;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 30px;
+    color: #6A6A6A;
 
     @include breakpoint-min(lg) {
-      margin-bottom: 7rem;
       text-align: start;
     }
   }
