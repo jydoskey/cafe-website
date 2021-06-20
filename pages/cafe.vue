8e71d1a8c59c2b5745b2dbafc69ce94ab033cb05
jide_omotola <jide@reach.africa>
2021-06-20 23:12:45 +0100

    added our menu section to cafe page

--- pages/cafe.vue
+++ pages/cafe.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <heroSection />
+    <ourMenu />
   </div>
 </template>
 <script>
   import heroSection from "@/components/Cafe/heroSection";
+  import ourMenu from "@/components/Cafe/ourMenu";
   export default {
     components: {
-      heroSection
+      heroSection,
+      ourMenu
     }
   }
 
