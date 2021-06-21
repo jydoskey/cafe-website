a70be37378a3776cfe992bebd2af03e36051d40a
jide_omotola <jide@reach.africa>
2021-06-21 01:20:11 +0100

    completed cafe menu section

---
+++ components/Cafe/ourMenu.vue
@@ -0,0 +1,518 @@
+<template>
+  <div class="cafes">
+    <section class="container">
+      <div class="home-section home-section-b">
+        <div class="text-center">
+          <h2>Our Menu</h2>
+          <img src="@/assets/img/cafe-menu.svg" alt="cafe menu">
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="cafe align-items-center justify-content-center">
+              <div class="col-lg-4 pt-3 com-col"><img class="cafe-img" src="@/assets/img/coffee.png" alt="regular-png">
+              </div>
+              <div class="cafe-text col-lg-7">
+                <h6 class="cafe-h6">
+                  Regular
+                </h6>
+                <div class="cafe-p">
+                  <table class="table table-borderless">
+                    <tbody>
+                      <tr>
+                        <th scope="row">Expresso</th>
+                        <td>S 700</td>
+                        <td>D 1200</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Americano</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Mocha</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Cappuchino</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Caramel Machiato</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Latte</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+            <div class="cafe-reverse align-items-center justify-content-center">
+              <div class="cafe-text col-lg-7">
+                <h6 class="cafe-h6">
+                  Cold Brew
+                </h6>
+                <div class="cafe-p">
+                  <table class="table table-borderless">
+                    <tbody>
+                      <tr>
+                        <th scope="row">Expresso</th>
+                        <td>S 700</td>
+                        <td>D 1200</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Americano</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Mocha</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Cappuchino</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Caramel Machiato</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Latte</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div class="col-lg-4 pt-3 com-rev-col">
+                <img class="cafe-img" src="@/assets/img/coffee.png" alt="col-brew-png">
+              </div>
+            </div>
+
+            <div class="cafe align-items-center justify-content-center">
+              <div class="col-lg-4 pt-3 com-col">
+                <img class="cafe-img" src="@/assets/img/coffee.png" alt="gourmet-png">
+              </div>
+              <div class="cafe-text col-lg-7">
+                <h6 class="cafe-h6">Gourmet</h6>
+                <div class="cafe-p">
+                  <table class="table table-borderless">
+                    <tbody>
+                      <tr>
+                        <th scope="row">Expresso</th>
+                        <td>S 700</td>
+                        <td>D 1200</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Americano</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Mocha</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Cappuchino</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Caramel Machiato</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Latte</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+            <div class="cafe-reverse align-items-center justify-content-center">
+              <div class="cafe-text col-lg-7">
+                <h6 class="cafe-h6">Specially Iced Latte</h6>
+                <div class="cafe-p">
+                  <table class="table table-borderless">
+                    <tbody>
+                      <tr>
+                        <th scope="row">Expresso</th>
+                        <td>S 700</td>
+                        <td>D 1200</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Americano</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Mocha</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Cappuchino</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Caramel Machiato</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Latte</th>
+                        <td>S 1000</td>
+                        <td>M 1200</td>
+                        <td>L 1600</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div class="col-lg-4 pt-3 com-rev-col mb-5">
+                <img class="cafe-img" src="@/assets/img/coffee.png" alt="latte-png">
+              </div>
+            </div>
+            <div class="text-center pt-2 mt-5 pt-5">
+              <button type="submit" id="submit" name="submit"
+                class="col-md-4 btn btn-lg btn-block btn-red mr-auto ml-auto">
+                PLACE ORDER
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+  export default {
+    name: "ourMenu",
+  };
+
+</script>
+
+
+<style lang='scss' scoped>
+  @import "@/scss/abstracts/_variables";
+  @import "@/scss/abstracts/_mixins";
+
+  .table {
+    border-collapse: separate;
+    border-spacing: 0 5px;
+  }
+
+  tr {
+    background: #FFF5E4;
+  }
+
+  td {
+    font-family: Lato;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 30px;
+    color: #222831;
+  }
+
+  th {
+    font-family: Lora;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 31px;
+    color: #474E59;
+  }
+
+  .cafe {
+    display: flex;
+    flex-direction: column;
+    padding-top: 5rem;
+
+    @include breakpoint-min(sm) {
+      max-width: unset;
+    }
+
+    @include breakpoint-min(md) {
+      flex-direction: row;
+      align-items: center;
+    }
+
+  }
+
+  .home-section h2 {
+    font-size: 40px;
+    font-weight: 800;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.18;
+    letter-spacing: normal;
+    color: #474E59;
+    text-align: start;
+    font-family: Lora;
+  }
+
+  .cafe-reverse {
+    display: flex;
+    flex-direction: column-reverse;
+    padding-top: 5rem;
+
+    @include breakpoint-min(sm) {
+      max-width: unset;
+    }
+
+    @include breakpoint-min(md) {
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+
+  .cafes {
+    background-color: #FFFFFF;
+    padding-bottom: 5rem;
+    padding-top: 4rem;
+  }
+
+  .container {
+    @include breakpoint-min(sm) {
+      min-width: 540px;
+      max-width: calc(100% - 70px);
+    }
+
+    @include breakpoint-min(md) {
+      max-width: calc(100% - 108px);
+      min-width: 720px;
+    }
+
+    @include breakpoint-min(lg) {
+      min-width: 960px;
+    }
+
+    @media (min-width:1400px) {
+      max-width: 1300px;
+    }
+  }
+
+  .row {
+    @media (min-width: 320px) {
+      flex-direction: column-reverse;
+    }
+
+    @media (min-width: 992px) {
+      flex-direction: row;
+    }
+
+    @media (min-width: 1200px) {
+      flex-direction: row;
+      margin-left: -15px;
+    }
+  }
+
+  .row-1 {
+    padding-bottom: 20px;
+  }
+
+  .cafe-text {
+    p {
+      font-size: 1rem;
+      font-weight: bold;
+    }
+  }
+
+  @media (max-width: 991px) {
+    .cafe-text {
+      grid-row: 1/3;
+    }
+  }
+
+  .home-section {
+    @media (min-width: 280px) {
+      padding: 0;
+    }
+
+    @include breakpoint-min(lg) {
+      padding-bottom: 2rem;
+    }
+
+    @media (min-width:1500px) {
+      padding-bottom: 3rem;
+    }
+  }
+
+
+
+  .home-section-b {
+    padding-bottom: 0;
+  }
+
+  .cafe-h6 {
+    color: #474E59;
+    font-family: Lora;
+    font-style: normal;
+    font-weight: bold;
+    line-height: 41px;
+
+    @media (min-width: 280px) {
+      font-size: 1.125rem;
+    }
+
+    @include breakpoint-min(md) {
+      font-size: 1.125rem;
+    }
+
+    @include breakpoint-min(lg) {
+      font-size: 1.5625rem;
+    }
+
+    @include breakpoint-min(xl) {
+      font-size: 1.875rem;
+    }
+  }
+
+  .cafe-p {
+    font-family: Lato;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 28px;
+    color: #6A6A6A;
+
+    @media (min-width: 280px) {
+      font-size: 0.75rem;
+    }
+
+    @include breakpoint-min(md) {
+      font-size: 0.875rem;
+    }
+
+    @include breakpoint-min(lg) {
+      font-size: 1rem;
+      max-width: 90%;
+    }
+
+    @include breakpoint-min(xl) {
+      font-size: 1.125rem;
+      max-width: 100%;
+    }
+  }
+
+  .p-style {
+    color: #303030;
+
+    @media (min-width: 280px) {
+      font-size: 0.75rem;
+    }
+
+    @media (min-width: 425px) {
+      font-size: 1rem;
+    }
+
+    @media (min-width: 576px) {
+      font-size: 1.125rem;
+    }
+
+    @media (min-width: 1200px) {
+      margin-bottom: 0.625rem;
+    }
+  }
+
+  .btn-red {
+    border-radius: 0% !important;
+    font-size: 16px;
+    height: 3.5rem;
+  }
+
+  .cafe-img {
+    width: 280px;
+    height: 330px;
+    z-index: 999;
+  }
+
+  .com-col {
+    position: relative;
+    z-index: 1;
+
+    &::after {
+      content: "";
+      position: absolute;
+      left: -1rem;
+      top: 5rem;
+      width: 280px;
+      height: 300px;
+      z-index: -1;
+      background: #FFF8ED;
+    }
+
+    @include breakpoint-max (md) {
+      &::after {
+        content: "";
+        background: #FFF5E4;
+      }
+    }
+  }
+
+  .com-rev-col {
+    position: relative;
+    z-index: 1;
+
+    &::after {
+      content: "";
+      position: absolute;
+      left: 2.8rem;
+      top: 5rem;
+      width: 280px;
+      height: 300px;
+      z-index: -1;
+      background: #FFF8ED;
+    }
+
+    @include breakpoint-max (md) {
+      &::after {
+        content: "";
+        background: #FFF5E4;
+      }
+    }
+  }
+
+</style>
